811b0c4e260ff78bbd853fe02b721d3ac984d6aa
PSILVA999 <p.silva0399@gmail.com>
2023-01-22 19:16:22 -0300

    header

--- 05-useState/src/Header.jsx
+++ 05-useState/src/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
     </header>
   )
 }
