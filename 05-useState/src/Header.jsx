7afac6253f32e02a3cb928e2ad514e54ecceed82
PSILVA999 <p.silva0399@gmail.com>
2023-01-26 21:05:36 -0300

    header

--- 05-useState/src/Header.jsx
+++ 05-useState/src/Header.jsx
@@ -6,7 +6,6 @@ const Header = () => {
       <li><a href="/home">home</a></li>
       <li><a href="/about">about</a></li>
       <li><a href="/contact">contact</a></li>
-      <li><a href="/contact">products</a></li>
     </header>
   )
 }
